8cdcb2b97ebe728b30ffa263547c4466ba6e0530
ericksloninski <erickslk@hotmail.com>
2024-07-22 20:58:44 -0300

    Fix database error

--- server/db.js
+++ server/db.js
@@ -2,15 +2,20 @@ import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
     try {
-        mongoose.set('strictQuery', false)
-        const connect = await mongoose.connect(process.env.MONGO_URI, {
+        mongoose.set('strictQuery', false);
+        const mongoUri = process.env.MONGO_URI;
+        if (!mongoUri) {
+            throw new Error("MONGO_URI não está definida");
+        }
+        console.log(`Tentando conectar ao MongoDB com a URI: ${mongoUri}`);
+        const connect = await mongoose.connect(mongoUri, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         });
-        console.log(`MongoDB Connected: ${connect.connection.host}`)
+        console.log(`MongoDB Connected: ${connect.connection.host}`);
     } catch (error) {
         console.log(`Error: ${error.message}`);
     }
-}
+};
 
 export default connectToDatabase;
